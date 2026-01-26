c6fff18a3b9bd0a4b2997ab395ca56a1e73f1362
JaineetSingh <Jstaneja2309@gmail.com>
2026-01-26 03:22:19 -0500

    Sample Test Prokect for Git Jenkin Integration

---
+++ test/tests.py
@@ -0,0 +1,15 @@
+# tests/test_app.py
+
+from main import add
+
+def test_add_positive_numbers():
+    assert add(1, 2) == 3
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+def test_add_zero():
+    assert add(0, 0) == 0
+
+def test_add_mixed_numbers():
+    assert add(5, -3) == 2
